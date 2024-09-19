77d9a6d47682754aaeec3974514bd70eabb88b0d
SURYA1804 <155455542+SURYA1804@users.noreply.github.com>
2024-09-19 21:12:53 +0530

    Add files via upload

---
+++ BusBackEnd/Program.cs
@@ -0,0 +1,29 @@
+using DotNet.API;
+using DotNet.Dbcontext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<MyDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("Bus")));
+var app = builder.Build();
+
+
+
+
+
+string ConnectionString = "host='127.0.0.1';database='postgres';user Id='postgres';password='Alterego@2';port = 5432;";
+app.CustomerApi(ConnectionString);
+app.DiscountOfferApi(ConnectionString);
+app.BookedSeatsApi(ConnectionString);
+app.BusApi(ConnectionString);
+app.SeatApi(ConnectionString);
+app.NewBookingApi(ConnectionString);
+app.RouteApi(ConnectionString);
+app.BoardingPointApi(ConnectionString);
+app.DropPointApi(ConnectionString);
+app.Run();
+
+
+
